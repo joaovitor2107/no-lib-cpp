52e2daf3165e9dffd0006575badd9221d6644a5f
Joao Vitor Trindade <jvtrindade21@joaos-mac-mini.local>
2026-07-21 00:02:32 -0300

    feat: adding resizing when map reaches max size

--- mylib/include/containers/my_map.hpp
+++ mylib/include/containers/my_map.hpp
@@ -5,20 +5,35 @@
 #include "my_pair.hpp"
 
 template<typename k, typename v>
-// adicionar resizing quando > O(1)
 class my_map {
     private:
-        // arbitrary number
-        static constexpr size_t num_buckets = 16;
+        // arbitrary number to start
+        size_t _num_buckets = 16;
+        size_t _count = 0;  // number of elements
+
         my_vector<my_vector<my_pair<k, v>>> _buckets;
 
         size_t get_hash(const k &key) {
-            return std::hash<k>{} (key) % num_buckets;
+            return std::hash<k>{} (key) % _num_buckets;
         }
 
+        void rehash() {
+            size_t new_num_buckets = _num_buckets * 2;
+            my_vector<my_vector<my_pair<k, v>>> new_buckets(new_num_buckets);
+
+            _num_buckets = new_num_buckets;
+            for (size_t i = 0; i < _buckets.size(); i++) {
+                for (size_t j = 0; j < _buckets[i].size(); j++) {
+                    auto& entry =  _buckets[i][j];
+                    size_t new_index = get_hash(entry.first);
+                    new_buckets[new_index].push_back(entry);
+                }
+            }
+
+        }
 
     public:
-        my_map() : _buckets(num_buckets) { }
+        my_map() : _buckets(_num_buckets) { }
 
 
         // alterar para ser atomico
@@ -56,7 +71,12 @@ class my_map {
                     return;
             }
             // new key
+            if (_count + 1 > _num_buckets) {
+                rehash();
+            }
+
             _buckets[get_hash(key)].push_back({key, val});
+            _count++;
         }
 
 
